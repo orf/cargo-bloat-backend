ef5db5f254017f6e5c7e2446ec60a99e81ce3452
Tom Forbes <tom@tomforb.es>
2020-01-05 22:16:49 +0000

    Ok

--- src/bin/install.rs
+++ src/bin/install.rs
@@ -30,21 +30,3 @@ fn handler(_: Request, _: Context) -> Result<impl IntoResponse, HandlerError> {
         "message": "Go Serverless v1.0! Your function executed successfully!"
     }))
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn handler_handles() {
-        let request = Request::default();
-        let expected = json!({
-            "message": "Go Serverless v1.0! Your function executed successfully!"
-        })
-        .into_response();
-        let response = handler(request, Context::default())
-            .expect("expected Ok(_) value")
-            .into_response();
-        assert_eq!(response.body(), expected.body())
-    }
-}
