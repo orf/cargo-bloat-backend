47414462729dda0c4478c77458f9cd437f892c6f
Tom Forbes <tom@tomforb.es>
2020-01-05 22:35:27 +0000

    Run migrations

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,27 @@
 #[macro_use]
 extern crate diesel;
+#[macro_use]
+extern crate diesel_migrations;
 
 pub mod models;
 pub mod schema;
 
 use diesel::pg::PgConnection;
+use diesel_migrations::embed_migrations;
 use diesel::prelude::*;
 use dotenv::dotenv;
 use std::env;
+
+embed_migrations!("migrations/");
 
 pub fn establish_connection() -> PgConnection {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+    let conn = PgConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
+    embedded_migrations::run_with_output(&conn, &mut std::io::stdout());
+    return conn;
 }
 
 #[cfg(test)]
