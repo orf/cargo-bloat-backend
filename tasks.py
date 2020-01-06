0395ce026a37fda9391b81a05976f4684c87a6f5
Tom Forbes <tom@tomforb.es>
2020-01-06 00:34:09 +0000

    Prod?

--- tasks.py
+++ tasks.py
@@ -4,4 +4,4 @@ from invoke import task, Context
 @task
 def deploy(c):
     c.run("poetry export -f requirements.txt -o requirements.txt --without-hashes", echo=True)
-    c.run("chalice deploy", pty=True, echo=True)
+    c.run("chalice deploy --stage=prod --api-gateway-stage=prod", pty=True, echo=True)
