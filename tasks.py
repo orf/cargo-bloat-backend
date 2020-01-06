58d48c3084555a2c92dcd4eda1a8217de82854d6
Tom Forbes <tom@tomforb.es>
2020-01-06 00:50:25 +0000

    Update deployments

--- tasks.py
+++ tasks.py
@@ -1,7 +1,16 @@
 from invoke import task, Context
+import tempfile
 
 
 @task
-def deploy(c):
+def deploy(c, env="prod"):
+    temp_dir = tempfile.mkdtemp()
     c.run("poetry export -f requirements.txt -o requirements.txt --without-hashes", echo=True)
-    c.run("chalice deploy --stage=prod --api-gateway-stage=prod", pty=True, echo=True)
+    c.run("poetry run chalice package %s" % temp_dir)
+    c.run("aws cloudformation package "
+          "--template-file=%s/sam.json "
+          "--s3-bucket=cargo-bloat-deploy "
+          "--output-template-file=%s/packaged.yaml" % (temp_dir, temp_dir),
+          pty=True, echo=True)
+    c.run("aws cloudformation deploy --template-file=%s/packaged.yml --stack-name=cargo-bloat-%s" % (temp_dir, env),
+          pty=True, echo=True)
