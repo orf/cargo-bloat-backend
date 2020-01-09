7e6f7073291b57c52b96c55dc48e197274e932b0
Tom Forbes <tom@tomforb.es>
2020-01-09 20:38:41 +0000

    Dont deploy

--- app.py
+++ app.py
@@ -1,34 +1,11 @@
-from chalice import Chalice
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.core import patch_all
-
-xray_recorder.configure()
-patch_all()
-
-app = Chalice(app_name='cargo-bloat-backend')
-
-
-@app.route('/')
-def index():
-    return {'hello': 'world'}
-
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+def hello_get(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
+    """
+    return 'Hello World!'
--- tasks.py
+++ tasks.py
@@ -1,18 +1,8 @@
 from invoke import task
-import tempfile
 
 
 @task
-def deploy(c, env="prod"):
-    temp_dir = tempfile.mkdtemp()
+def deploy(c):
     c.run("poetry export -f requirements.txt -o requirements.txt --without-hashes", echo=True)
-    c.run("poetry run chalice package %s" % temp_dir)
-    c.run("aws cloudformation package "
-          "--template-file=%s/sam.json "
-          "--s3-bucket=cargo-bloat-deploy "
-          "--output-template-file=%s/packaged.yaml" % (temp_dir, temp_dir),
-          pty=True, echo=True)
-    c.run("aws cloudformation deploy --template-file=%s/packaged.yaml "
-          "--stack-name=cargo-bloat-%s "
-          "--capabilities CAPABILITY_IAM" % (temp_dir, env),
+    c.run("gcloud functions deploy cargo-bloat --runtime python37 --trigger-http",
           pty=True, echo=True)
