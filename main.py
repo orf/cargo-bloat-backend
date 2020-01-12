7e9045c6958c0470043e36c029e1dc05f8f286d2
Tom Forbes <tom@tomforb.es>
2020-01-12 01:31:01 +0000

    Remove webhook

--- main.py
+++ main.py
@@ -4,46 +4,6 @@ from google.cloud.datastore import Client, Entity
 client = Client()
 
 
-def handle_installation(data: dict):
-    action = data["action"]
-    install_id = data["installation"]["id"]
-    install_key = client.key("Installation", install_id)
-    if action == "deleted":
-        client.delete(install_key)
-        return jsonify(status="deleted install")
-    elif action == "created":
-        repo_names = [r["full_name"] for r in data["repositories"]]
-        install_object = Entity(key=install_key, exclude_from_indexes=("account",))
-        install_object["repos"] = repo_names
-        install_object["user"] = data["installation"]["account"]["login"]
-        client.put(install_object)
-        return jsonify(status="created install")
-    return jsonify(error=f"unhandled action {action}"), 400
-
-
-def handle_installation_repositories(data: dict):
-    install_id = data["installation"]["id"]
-    install_key = client.key("Installation", install_id)
-    added = {r["full_name"] for r in data["repositories_added"]}
-    removed = {r["full_name"] for r in data["repositories_removed"]}
-    install_object = client.get(install_key)
-    install_object["repos"] = list((set(install_object["repos"]) | added) - removed)
-    client.put(install_object)
-    return jsonify(status="updated install")
-
-
-def webhook(request: Request):
-    data = request.get_json()
-    event = request.headers["X-GitHub-Event"]
-
-    if event == "installation":
-        return handle_installation(data)
-    elif event == "installation_repositories":
-        return handle_installation_repositories(data)
-    else:
-        return jsonify(status="unhandled")
-
-
 def fetch(request: Request):
     return "Hello World!"
 
--- tasks.py
+++ tasks.py
@@ -5,7 +5,6 @@ from invoke import task
 def deploy(c):
     commands = (
         "poetry export -f requirements.txt -o requirements.txt --without-hashes",
-        "gcloud --quiet functions deploy webhook --runtime python37 --trigger-http",
         "gcloud --quiet functions deploy fetch --runtime python37 --trigger-http",
         "gcloud --quiet functions deploy ingest --runtime python37 --trigger-http",
     )
