e45e981f2e077b7de3797547a9eb91d3b0c7c401
Tom Forbes <tom@tomforb.es>
2020-01-12 12:24:21 +0000

    Store and retrieve data

--- main.py
+++ main.py
@@ -5,8 +5,26 @@ client = Client()
 
 
 def fetch(request: Request):
-    return jsonify(ok=True)
+    repo = request.args["repo"]
+    repo_key = client.key("Bloat", repo)
+    entity = client.get(repo_key)
+    return jsonify(result=entity)
 
 
 def ingest(request: Request):
+    data = request.get_json()
+    repo = data["repo"]
+    repo_key = client.key("Bloat", repo)
+    crates = [(c["name"], c["size"]) for c in data["crates"]]
+    data = {
+        "commit": data["commit"],
+        "file_size": data["file_size"],
+        "text_size": data["text_size"],
+        "toolchain": data["toolchain"],
+        "rustc": data["rustc"],
+        "bloat": data["bloat"],
+        "crates": crates,
+    }
+    entity = Entity(repo_key, exclude_from_indexes=tuple(data.keys()))
+    client.put(entity)
     return jsonify(ok=True)
