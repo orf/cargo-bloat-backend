86a096e3c45518cf736f1c6d3aa36758c12c5376
Tom Forbes <tom@tomforb.es>
2020-01-12 00:39:55 +0000

    Re

--- tasks.py
+++ tasks.py
@@ -3,6 +3,15 @@ from invoke import task
 
 @task
 def deploy(c):
-    c.run("poetry export -f requirements.txt -o requirements.txt --without-hashes", echo=True)
-    c.run("gcloud --quiet functions deploy webhook --runtime python37 --trigger-http", echo=True)
-    c.run("gcloud --quiet functions deploy fetch --runtime python37 --trigger-http", echo=True)
+    c.run(
+        "poetry export -f requirements.txt -o requirements.txt --without-hashes",
+        echo=True,
+    )
+    c.run(
+        "gcloud --quiet functions deploy webhook --runtime python37 --trigger-http",
+        echo=True,
+    )
+    c.run(
+        "gcloud --quiet functions deploy fetch --runtime python37 --trigger-http",
+        echo=True,
+    )
